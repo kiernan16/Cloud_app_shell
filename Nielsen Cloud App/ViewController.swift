3ce00422553585856d26bfec32186c2966e072a9
Matt Kiernan <matt.kiernan@nielsen.com>
2017-04-09 13:40:25 -0400

    Initial Commit

---
+++ Nielsen Cloud App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nielsen Cloud App
+//
+//  Created by Nielsen on 4/9/17.
+//  Copyright © 2017 Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
